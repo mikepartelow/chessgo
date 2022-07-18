42ef49a978f7d8472162c8e7695e602bd0206001
Mike Partelow <github@mikepartelow.com>
2022-07-18 13:41:56 -0700

    tests

--- game_immortal_test.go
+++ game_immortal_test.go
@@ -31,8 +31,9 @@ func Test(t *testing.T) {
 			wantBoard: "RNBQKBNRPPPP  PP            PP      p           pppp ppprnbqkbnr",
 		},
 		{
-			move:      "exf4",
-			wantBoard: "RNBQKBNRPPPP  PP            Pp                  pppp ppprnbqkbnr",
+			move:         "exf4",
+			wantBoard:    "RNBQKBNRPPPP  PP            Pp                  pppp ppprnbqkbnr",
+			wantCaptured: chessgo.WhitePawn,
 		},
 		{
 			move:      "Bc4",
@@ -52,8 +53,9 @@ func Test(t *testing.T) {
 			wantBoard: "RNBQ KNRPPPP  PP          B Pp q p              p pp ppprnb kbnr",
 		},
 		{
-			move:      "Bxb5",
-			wantBoard: "RNBQ KNRPPPP  PP            Pp q B              p pp ppprnb kbnr",
+			move:         "Bxb5",
+			wantBoard:    "RNBQ KNRPPPP  PP            Pp q B              p pp ppprnb kbnr",
+			wantCaptured: chessgo.BlackPawn,
 		},
 		{
 			move:      "Nf6",
@@ -103,6 +105,94 @@ func Test(t *testing.T) {
 			move:      "Rg1",
 			wantBoard: "RNBQ KR PPP    P   P        PpP  B   Nq   p  n  p  p ppprnb kb r",
 		},
+		{
+			move:         "cxb5",
+			wantBoard:    "RNBQ KR PPP    P   P        PpP  p   Nq      n  p  p ppprnb kb r",
+			wantCaptured: chessgo.WhiteBishop,
+		},
+		{
+			move:      "h4",
+			wantBoard: "RNBQ KR PPP        P        PpPP p   Nq      n  p  p ppprnb kb r",
+		},
+		{
+			move:      "Qg6",
+			wantBoard: "RNBQ KR PPP        P        PpPP p   N       nq p  p ppprnb kb r",
+		},
+		{
+			move:      "h5",
+			wantBoard: "RNBQ KR PPP        P        PpP  p   N P     nq p  p ppprnb kb r",
+		},
+		{
+			move:      "Qg5",
+			wantBoard: "RNBQ KR PPP        P        PpP  p   NqP     n  p  p ppprnb kb r",
+		},
+		{
+			move:      "Qf3",
+			wantBoard: "RNB  KR PPP        P Q      PpP  p   NqP     n  p  p ppprnb kb r",
+		},
+		{
+			move:      "Ng8",
+			wantBoard: "RNB  KR PPP        P Q      PpP  p   NqP        p  p ppprnb kbnr",
+		},
+		{
+			move:         "Bxf4",
+			wantBoard:    "RN   KR PPP        P Q      PBP  p   NqP        p  p ppprnb kbnr",
+			wantCaptured: chessgo.BlackPawn,
+		},
+		{
+			move:      "Qf6",
+			wantBoard: "RN   KR PPP        P Q      PBP  p   N P     q  p  p ppprnb kbnr",
+		},
+		{
+			move:      "Nc3",
+			wantBoard: "R    KR PPP       NP Q      PBP  p   N P     q  p  p ppprnb kbnr",
+		},
+		{
+			move:      "Bc5",
+			wantBoard: "R    KR PPP       NP Q      PBP  pb  N P     q  p  p ppprnb k nr",
+		},
+		{
+			move:      "Nd5",
+			wantBoard: "R    KR PPP        P Q      PBP  pbN N P     q  p  p ppprnb k nr",
+		},
+		{
+			move:         "Qxb2",
+			wantBoard:    "R    KR PqP        P Q      PBP  pbN N P        p  p ppprnb k nr",
+			wantCaptured: chessgo.WhitePawn,
+		},
+		{
+			move:      "Bd6",
+			wantBoard: "R    KR PqP        P Q      P P  pbN N P   B    p  p ppprnb k nr",
+		},
+		{
+			move:         "Bxg1",
+			wantBoard:    "R    Kb PqP        P Q      P P  p N N P   B    p  p ppprnb k nr",
+			wantCaptured: chessgo.WhiteRook,
+		},
+		{
+			move:      "e5",
+			wantBoard: "R    Kb PqP        P Q        P  p NPN P   B    p  p ppprnb k nr",
+		},
+		{
+			move:         "Qxa1+",
+			wantBoard:    "q    Kb P P        P Q        P  p NPN P   B    p  p ppprnb k nr",
+			wantCheck:    true,
+			wantCaptured: chessgo.WhiteRook,
+		},
+		{
+			move:      "Ke2",
+			wantBoard: "q     b P P K      P Q        P  p NPN P   B    p  p ppprnb k nr",
+		},
+		{
+			move:      "Na6",
+			wantBoard: "q     b P P K      P Q        P  p NPN Pn  B    p  p pppr b k nr",
+		},
+		// {
+		// 	move:         "Nxg7+",
+		// 	wantBoard:    "q     b P P K      P Q        P  p NP  Pn  B    p  p pNpr b k nr",
+		// 	wantCheck:    true,
+		// 	wantCaptured: chessgo.BlackPawn,
+		// },
 	}
 
 	board := chessgo.NewBoard()
@@ -117,7 +207,11 @@ func Test(t *testing.T) {
 				t.Fatalf("Unexpected error %v", err)
 			}
 
-			if tC.wantCaptured != chessgo.Piece(0) && captured != tC.wantCaptured {
+			if tC.wantCaptured == chessgo.Piece(0) {
+				if captured != chessgo.NoPiece {
+					t.Fatalf("captured %q but was expecting no capture", captured)
+				}
+			} else if captured != tC.wantCaptured {
 				t.Fatalf("Wanted %q got %q", tC.wantCaptured, captured)
 			}
 
